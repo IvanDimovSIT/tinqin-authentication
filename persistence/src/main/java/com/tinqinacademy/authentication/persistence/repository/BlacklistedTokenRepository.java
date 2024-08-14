d0d8eeabbcb1834a98fcd7a5641bfa91ccef1b3a
IvanDimovSIT <ivan390@abv.bg>
2024-08-14 14:06:31 +0300

    Added BlacklistedTokensRemover

--- persistence/src/main/java/com/tinqinacademy/authentication/persistence/repository/BlacklistedTokenRepository.java
+++ persistence/src/main/java/com/tinqinacademy/authentication/persistence/repository/BlacklistedTokenRepository.java
@@ -2,11 +2,17 @@ package com.tinqinacademy.authentication.persistence.repository;
 
 import com.tinqinacademy.authentication.persistence.model.BlacklistedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BlacklistedTokenRepository extends JpaRepository<BlacklistedToken, UUID> {
+
+    @Query("SELECT b FROM BlacklistedToken b WHERE b.created + 5 MINUTE < CURRENT_DATE")
+    List<BlacklistedToken> getOldTokens();
+
     boolean existsByToken(String token);
 }
--- rest/src/main/java/com/tinqinacademy/authentication/rest/AuthenticationApplication.java
+++ rest/src/main/java/com/tinqinacademy/authentication/rest/AuthenticationApplication.java
@@ -1,16 +1,14 @@
 package com.tinqinacademy.authentication.rest;
 
-import com.tinqinacademy.authentication.core.email.AuthenticationMailSender;
-import com.tinqinacademy.authentication.core.email.implementation.AuthenticationMailSenderImpl;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan(basePackages = "com.tinqinacademy.authentication")
 @EnableJpaRepositories(basePackages = "com.tinqinacademy.authentication.persistence.repository")
 @EntityScan(basePackages = "com.tinqinacademy.authentication.persistence.model")
