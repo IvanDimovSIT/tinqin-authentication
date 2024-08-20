d2415dfcb757eb2b882dd79974f8085d851cd48f
Ivan Dimov <100278096+IvanDimovSIT@users.noreply.github.com>
2024-08-20 11:58:33 +0300

    [T18] Added Retryable to AuthenticationMailSenderImpl (#36)

--- core/src/main/java/com/tinqinacademy/authentication/core/email/implementation/AuthenticationMailSenderImpl.java
+++ core/src/main/java/com/tinqinacademy/authentication/core/email/implementation/AuthenticationMailSenderImpl.java
@@ -4,10 +4,15 @@ import com.tinqinacademy.authentication.core.email.AuthenticationMailSender;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @Slf4j
@@ -19,6 +24,11 @@ public class AuthenticationMailSenderImpl implements AuthenticationMailSender {
 
     @Override
     @Async
+    @Retryable(
+            value = { Exception.class },
+            maxAttempts = 5,
+            backoff = @Backoff(delay = 2000)
+    )
     public void send(String to, String subject, String contents) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(from);
@@ -26,7 +36,11 @@ public class AuthenticationMailSenderImpl implements AuthenticationMailSender {
         message.setSubject(subject);
         message.setText(contents);
         emailSender.send(message);
-
         log.info("Email sent to {} from {}", to, from);
+    }
+
+    @Recover
+    public void recover(Exception exception, String to, String subject, String contents) {
+        log.error("Error:{} Email failed to send to {} from {}", exception.getMessage(), to, from);
     }
 }
--- rest/src/main/java/com/tinqinacademy/authentication/rest/AuthenticationApplication.java
+++ rest/src/main/java/com/tinqinacademy/authentication/rest/AuthenticationApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @ComponentScan(basePackages = "com.tinqinacademy.authentication")
 @EnableJpaRepositories(basePackages = "com.tinqinacademy.authentication.persistence.repository")
 @EntityScan(basePackages = "com.tinqinacademy.authentication.persistence.model")
+@EnableRetry
 public class AuthenticationApplication {
 
     public static void main(String[] args) {
